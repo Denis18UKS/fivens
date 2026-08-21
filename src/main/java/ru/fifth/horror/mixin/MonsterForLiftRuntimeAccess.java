0ca34e66900cac8337896853f93b8f9060c86c36
Denis18UKS <148848338+Denis18UKS@users.noreply.github.com>
2026-08-21 20:37:39 +0500

    Expose MFL runtime hooks for test controls

---
+++ src/main/java/ru/fifth/horror/mixin/MonsterForLiftRuntimeAccess.java
@@ -0,0 +1,23 @@
+package ru.fifth.horror.mixin;
+
+import net.minecraft.server.network.ServerPlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.gen.Invoker;
+import ru.fifth.horror.entity.MonsterForLiftEntity;
+
+/** Narrow server-side access used by the authored MFL runtime/test controller. */
+@Mixin(MonsterForLiftEntity.class)
+public interface MonsterForLiftRuntimeAccess {
+    @Accessor("manualAnimationTicks")
+    int fiven$getManualAnimationTicks();
+
+    @Accessor("manualAnimationTicks")
+    void fiven$setManualAnimationTicks(int ticks);
+
+    @Invoker("setCurrentAnimation")
+    void fiven$setCurrentAnimation(String animation);
+
+    @Invoker("canSeeTarget")
+    boolean fiven$canSeeTarget(ServerPlayerEntity player);
+}
